0f70ffb31c1409c7c3903961c34bafb9e5041cb4
Brian O'Meara <omeara.brian@gmail.com>
2016-05-06 22:22:47 -0400

    rorcid now works, using for getting funding, education, and pubs

--- CreateCV.R
+++ CreateCV.R
@@ -1,10 +1,33 @@
+
+
 GetInfoFromOrcid <- function(id="0000-0002-0337-5997") {
   me <- rorcid::orcid_id(id)
   me.pubs <- rorcid::works(me)$data
   # TO DO: Make sure all entries are bibtex
   journals <- me.pubs$'work-citation.citation'[which(me.pubs$'work-type'=="JOURNAL_ARTICLE")]
   books <- me.pubs$'work-citation.citation'[which(me.pubs$'work-type'=="BOOK")]
+  activities <- me[[1]]$`orcid-activities`
+  funding <- activities$`funding-list`$funding
+  affiliations <- activities$`affiliations`$affiliation
+  education <- subset(affiliations, type=="EDUCATION")
+  education <- education[order(education$'end-date.year.value', decreasing=TRUE),]
 
+  employment <- subset(affiliations, type=="EMPLOYMENT")
+  employment <- employment[order(employment $'start-date.year.value', decreasing=TRUE),]
+  
+  return(list(journals=journals, books=books, funding=funding, education=education, employment=employment))
+
+}
+
+CreateMarkdown <- function(orcid.info = GetInfoFromOrcid(), outdir=".") {
+		education.string <- ''
+		for (i in sequence(dim(orcid.info$education)[1])) {
+			education.string <- paste(education.string, '\n\n', 	orcid.info$education[i,]$organization.name, '\n\n', orcid.info$education[i,]$'role-title', sep='')
+			if(!is.na(education$'department-name')[i]) {
+				education.string <- paste(education.string, ' in ', 	orcid.info$education[i,]$'department-name', sep='')
+			}
+		}
+		cat(education.string, file=paste(outdir, "/education.md", sep=""))
 }
 
 FinalCompileCV <- function(input = c("head.md", "summary.md", "education.md", "employment.md", "publications.md", "teaching.md", "grants.md", "service.md", "postdocs.md", "gradstudents.md", "undergradstudents.md", "gradcommittees.md", "software.md", "talks.md"), output="OMearaCV.pdf") {
