4a40aafb63ed4dc692fbb95148cd7d6ea3d87db7
Brian O'Meara <omeara.brian@gmail.com>
2016-07-03 12:36:26 -0400

    actual R code

---
+++ R/CreateCV.R
@@ -0,0 +1,90 @@
+
+#' Pull in info from ORCID
+#' @param id Your ORCID id
+#' @return A list of info from ORCID
+#' @export
+GetInfoFromOrcid <- function(id="0000-0002-0337-5997") {
+  me <- rorcid::orcid_id(id)
+  me.pubs <- rorcid::works(me)$data
+  # TO DO: Make sure all entries are bibtex
+  journals <- me.pubs$'work-citation.citation'[which(me.pubs$'work-type'=="JOURNAL_ARTICLE")]
+  books <- me.pubs$'work-citation.citation'[which(me.pubs$'work-type'=="BOOK")]
+  activities <- me[[1]]$`orcid-activities`
+  funding <- activities$`funding-list`$funding
+  affiliations <- activities$`affiliations`$affiliation
+  education <- subset(affiliations, type=="EDUCATION")
+  education <- education[order(education$'end-date.year.value', decreasing=TRUE),]
+
+  employment <- subset(affiliations, type=="EMPLOYMENT")
+  employment <- employment[order(employment $'start-date.year.value', decreasing=TRUE),]
+
+  return(list(journals=journals, books=books, funding=funding, education=education, employment=employment))
+
+}
+
+#' Create a Markdown document from biographical info
+#' @param orcid.info The list of info from orcid
+#' @param outdir The directory to store the markdown file in
+#' @export
+CreateMarkdown <- function(orcid.info = GetInfoFromOrcid(), outdir=".") {
+	CreateEducationMarkdown(orcid.info, outdir)
+	CreateEmploymentMarkdown(orcid.info, outdir)
+
+
+}
+
+#' Create a Markdown document of education from biographical info
+#' @param orcid.info The list of info from orcid
+#' @param outdir The directory to store the markdown file in
+#' @export
+CreateEducationMarkdown <- function(orcid.info, outdir=".") {
+		education.string <- '## Education'
+		for (i in sequence(dim(orcid.info$education)[1])) {
+			education.string <- paste(education.string, '\n\n', 	orcid.info$education[i,]$organization.name, ': ', orcid.info$education[i,]$'role-title', " (", orcid.info$education[i,]$'end-date.year.value', ")", sep='')
+			if(!is.na(education$'department-name')[i]) {
+				education.string <- paste(education.string, ' in ', 	orcid.info$education[i,]$'department-name', sep='')
+			}
+		}
+		cat(education.string, file=paste(outdir, "/education.md", sep=""))
+}
+
+#' Create a Markdown document of employment history from biographical info
+#' @param orcid.info The list of info from orcid
+#' @param outdir The directory to store the markdown file in
+#' @export
+CreateEmploymentMarkdown <- function(orcid.info, outdir=".") {
+		employment.string <- '## Employment'
+		orcid.info$employment$'end-date.year.value'[which(is.na(employment$'end-date.year.value'))] <- "Present"
+		for (i in sequence(dim(orcid.info$employment)[1])) {
+			employment.string <- paste(employment.string, '\n\n', 	orcid.info$employment[i,]$'start-date.year.value', '-', orcid.info$employment[i,]$'end-date.year.value', ": ", orcid.info$employment[i,]$'role-title', ", ", sep='')
+			if(!is.na(employment$'department-name')[i]) {
+				employment.string <- paste(employment.string, "Dept. of ", 	orcid.info$employment[i,]$'department-name', ", ", sep='')
+			}
+			employment.string <- paste(employment.string, orcid.info$employment[i,]$'organization.name', ", ", orcid.info$employment[i,]$'organization.address.city', ", ", orcid.info$employment[i,]$'organization.address.region', sep="")
+		}
+		cat(employment.string, file=paste(outdir, "/employment.md", sep=""))
+}
+
+
+#' Create a Markdown document of funding from biographical info
+#' @param orcid.info The list of info from orcid
+#' @param outdir The directory to store the markdown file in
+#' @export
+CreateFundingMarkdown <- function(orcid.info, outdir=".", additional.te = "This is all in addition to other **funding my students have gotten** (NSF EAPSI grant, fellowships from NIMBioS and PEER (an NIH-funded program at UTK), Google Summer of Code funding), **funding for workshops or working groups** (from NIMBioS and the Society for Systematic Biologists), and **funding I got before my faculty position** (NESCent postdoctoral fellowship, NSF DDIG, NSF GRFP, and various internal grants at UC Davis).") {
+		funding.string <- '## Funding'
+		for (i in sequence(dim(orcid.info$funding)[1])) {
+			funding.string <- paste(funding.string, '\n\n', 	orcid.info$funding[i,]$organization.name, ': ', orcid.info$funding[i,]$'role-title', " (", orcid.info$funding[i,]$'end-date.year.value', ")", sep='')
+			if(!is.na(funding$'department-name')[i]) {
+				funding.string <- paste(funding.string, ' in ', 	orcid.info$funding[i,]$'department-name', sep='')
+			}
+		}
+		cat(funding.string, file=paste(outdir, "/funding.md", sep=""))
+}
+
+#' Compile a set of markdown documents and convert with pandoc
+#' @param input Vector of markdown documents
+#' @param output The output file name. Pandoc will use the extension info to create a file of the right type.
+#' @export
+FinalCompileCV <- function(input = c("head.md", "summary.md", "education.md", "employment.md", "publications.md", "teaching.md", "funding.md", "service.md", "postdocs.md", "gradstudents.md", "undergradstudents.md", "gradcommittees.md", "software.md", "talks.md"), output="OMearaCV.pdf") {
+  system(paste("pandoc ", paste(input, collapse=" "), " > ", output, sep=""))
+}
